d362a1614dae20cfc6683c410d81ec99f2a363a5
PlutoDDD <136102400+PlutoDDD@users.noreply.github.com>
2023-10-28 09:35:12 +0800

    main.py

--- main.py
+++ main.py
@@ -165,15 +165,15 @@ if __name__ == "__main__":
 
     # 阅览室晚上9点开始预约，自习室晚上8点半开始预约
 
-    #if( "自习室" not in cfg[key]["type"]) and ( "电子阅览室" not in cfg[key]["type"]):
+    if( "自习室" not in cfg[key]["type"]) and ( "电子阅览室" not in cfg[key]["type"]):
         # 阅览室
-        #if datetime.now().hour <= 20 - time_zone or datetime.now().hour == 20 - time_zone and datetime.now().minute < 30:  # github action cron定时有波动
-            #print("阅览室预约于21点开始预约，现在还未到预约时间，请检查下一个Action")
-            #exit(0)
-    #else:
-        #if datetime.now().hour > 20 - time_zone and datetime.now().minute > 30:
-            #print("自习室已于上个Action预约，请检查上一个预约")
-            #exit(0)
+        if datetime.now().hour <= 20 - time_zone or datetime.now().hour == 20 - time_zone and datetime.now().minute < 30:  # github action cron定时有波动
+            print("阅览室预约于21点开始预约，现在还未到预约时间，请检查下一个Action")
+            exit(0)
+    else:
+        if datetime.now().hour > 20 - time_zone and datetime.now().minute > 30:
+            print("自习室已于上个Action预约，请检查上一个预约")
+            exit(0)
 
     print("尝试预约,开始时间：{}，持续时间：{}小时".format(cfg[key]['开始时间'], cfg[key]['持续小时数']))
 
